f77f941a7928e7841889a35c2484ac48f63ab293
グスタボGustavo <juvinogustavo1@gmail.com>
2022-06-27 21:53:41 -0300

    :construction: Working in JS

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// Login And Register Navigation
+
 const sectionLogin = document.querySelector("[data-login]");
 const sectionRegister = document.querySelector("[data-register]");
 const register = document.querySelector("[data-span='register']");
 const login = document.querySelector("[data-span='login']");
 
-console.log(login)
 
 register.addEventListener("click", () =>{
     sectionRegister.classList.add("register-active");
@@ -13,4 +14,13 @@ register.addEventListener("click", () =>{
 login.addEventListener("click", () =>{
     sectionLogin.classList.add("login-active");
     sectionRegister.classList.remove("register-active");
+})
+
+// Activating Float Language-Menu
+const menuArrow = document.querySelector(".ArrowDown");
+const menuFloat = document.querySelector("[data-menu]");
+
+
+menuArrow.addEventListener("click", () =>{
+    menuFloat.classList.toggle("menu-floating");
 })
